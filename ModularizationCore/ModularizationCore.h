571588c233f0137afd8e32793fae37772e5b3fe8
Matheus <matheusramossilva999@gmail.com>
2021-02-07 23:40:20 -0300

    initial commit

---
+++ ModularizationCore/ModularizationCore.h
@@ -0,0 +1,18 @@
+//
+//  ModularizationCore.h
+//  ModularizationCore
+//
+//  Created by Matheus Ramos on 07/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModularizationCore.
+FOUNDATION_EXPORT double ModularizationCoreVersionNumber;
+
+//! Project version string for ModularizationCore.
+FOUNDATION_EXPORT const unsigned char ModularizationCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModularizationCore/PublicHeader.h>
+
+
